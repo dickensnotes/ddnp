fe5faa32eef0e06f01599f765e413de65aeb2671
Scott Bailey <csb5t@virginia.edu>
2022-10-04 07:12:20 -0400

    Add built-in TOC - still to be styled

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,19 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 import image from "@astrojs/image";
+import rehypeToc from "@jsdevtools/rehype-toc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), react(), tailwind(), mdx(), image()],
+  integrations: [
+    preact(),
+    react(),
+    tailwind(),
+    mdx({
+      rehypePlugins: [rehypeToc],
+    }),
+    image(),
+  ],
   vite: {
     // define: {
     //   global: 'window',
